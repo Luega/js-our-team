30929da2c0a72226aeff7dc192ebd5287c7b2193
Luega <martinelliluca90@gmail.com>
2021-12-09 15:36:33 +0100

    added cards

--- js/script.js
+++ js/script.js
@@ -30,3 +30,27 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+console.log(team);
+// Prendendo come riferimento la card di esempio presente nell’html, stampiamo dinamicamente una card per ogni membro del team.
+
+// dichiaro l´elemento container
+const teamContainer = document.querySelector(".team-container");
+
+// creo un ciclo for per creare le carte
+for (let i = 0; i < team.length; i++) {
+  teamContainer.innerHTML += 
+  `
+  <div class="team-card">
+    <div class="card-image">
+      <img
+        src="img/wayne-barnett-founder-ceo.jpg"
+        alt="Wayne Barnett"
+      />
+    </div>
+    <div class="card-text">
+      <h3>Wayne Barnett</h3>
+      <p>Founder & CEO</p>
+    </div>
+  </div>
+  `;
+}
